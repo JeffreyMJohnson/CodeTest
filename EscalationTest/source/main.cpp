a5ccc8f9a6ff2708f8718b443d5fc4ee33abd47a
JeffreyMJohnson <jefjohms@gmail.com>
2015-06-11 17:50:05 -0700

    have file loading and entities created.

--- EscalationTest/source/main.cpp
+++ EscalationTest/source/main.cpp
@@ -10,7 +10,12 @@ struct Vector2
 {
 	float x, y;
 
-	Vector2(){};
+	Vector2()
+	{
+		x = 0.0f;
+		y = 0.0f;
+	}
+
 	Vector2(float a_x, float a_y)
 	{
 		x = a_x;
@@ -21,9 +26,9 @@ struct Vector2
 class Entity
 {
 public:
-	enum Type 
+	enum Type : Ushort
 	{
-		HEALTH_PICKUP = -1,
+		HEALTH_PICKUP = 0,
 		CHEST = 1,
 		TRAP = 2,
 		TROLL = 3,
@@ -32,55 +37,87 @@ public:
 	};
 
 	Ushort GetID(){ return mID; }
-	Type GetTypeID(){ return mTypeID; }
+	Entity::Type GetTypeID(){ return mTypeID; }
 	float GetXPosition(){ return mPosition.x; }
 	float GetYPosition(){ return mPosition.y; }
 	float GetXDirection(){ return mDirection.x; }
 	float GetYDirection(){ return mDirection.y; }
+	float GetCollisionRadius(){ return mCollisionRadius; }
+	float GetFOV(){ return mFOV; }
 
-	Entity(Ushort id, Type type, float positionX, float positionY, float directionX, float directionY)
+	Entity(Ushort id, Entity::Type type, float positionX, float positionY, float directionX, float directionY, float collisionRadius)
 	{
 		mID = id;
 		mTypeID = type;
 		mPosition = Vector2(positionX, positionY);
 		mDirection = Vector2(directionX, directionY);
-
+		mCollisionRadius = collisionRadius;
+		mFOV = LoadFOV();
 	}
 
 	void PrintIt()
 	{
 		cout << "Entity [";
-		cout << "ID: " << mID;
-		cout << " Type: " << GetTypeName(mTypeID).c_str();
-		cout << " Location: (" << mPosition.x << ", " << mPosition.y << ")";
-		cout << " Direction: (" << mDirection.x << ", " << mDirection.y << ")";
+		cout << "ID:" << mID;
+		cout << " Entity::Type:" << GetTypeName(mTypeID).c_str();
+		cout << " Location:(" << mPosition.x << ", " << mPosition.y << ")";
+		cout << " Direction:(" << mDirection.x << ", " << mDirection.y << ")";
+		cout << " Collision Radius:" << mCollisionRadius;
+		cout << " FOV:" << mFOV << "]\n";
+	}
+
+	bool IsCollided(Entity& otherEntity)
+	{
+		//(x2-x1)^2 + (y1-y2)^2 <= (r1+r2)^2
+		return (((otherEntity.GetXPosition() - mPosition.x) * (otherEntity.GetXPosition() - mPosition.x)) + ((otherEntity.GetYPosition() - mPosition.y) * (otherEntity.GetYPosition() - mPosition.y))) <
+			(mCollisionRadius + GetCollisionRadius()) * (mCollisionRadius + GetCollisionRadius());
 	}
 
 
 private:
 	Ushort mID;
-	Type mTypeID;
+	Entity::Type mTypeID;
 	Vector2 mPosition;
 	Vector2 mDirection;
+	float mCollisionRadius;
+	float mFOV;
 
-	string GetTypeName(Type type)
+	string GetTypeName(Entity::Type type)
 	{
 		switch (type)
 		{
-		case Type::CHEST:
+		case Entity::Type::CHEST:
 			return "Chest";
-		case Type::HEALTH_PICKUP:
+		case Entity::Type::HEALTH_PICKUP:
 			return "Health Pickup";
-		case Type::IMP:
+		case Entity::Type::IMP:
 			return "Imp";
-		case Type::OGRE:
+		case Entity::Type::OGRE:
 			return "Ogre";
-		case Type::TRAP:
+		case Entity::Type::TRAP:
 			return "Trap";
-		case Type::TROLL:
+		case Entity::Type::TROLL:
 			return "Troll";
 		}
 		return "";
+	}
+
+	float LoadFOV()
+	{
+		if ((unsigned int)mTypeID > 2)
+		{
+			switch (mTypeID)
+			{
+			case Entity::Type::TROLL:
+				return 45.0f;
+			case Entity::Type::IMP:
+				return 65.0f;
+			case Entity::Type::OGRE:
+				return 90.0f;
+			}
+			cout << "Could not load FOV from type.";
+		}
+		return 0.0f;
 	}
 };
 
@@ -89,15 +126,100 @@ void LoadFile()
 
 }
 
-vector<Entity> entities;
+vector<Entity*> entities;
+
+float GetCollisionRadius(Entity::Type entityType)
+{
+	switch (entityType)
+	{
+	case Entity::Type::HEALTH_PICKUP:
+		return 10;
+	case Entity::Type::CHEST:
+		return 20;
+	case Entity::Type::TRAP:
+		return 64;
+	case Entity::Type::TROLL:
+		return 12;
+	case Entity::Type::IMP:
+		return 10;
+	case Entity::Type::OGRE:
+		return 25;
+	}
+	cout << "Could not get collision radius for entity.\n";
+	return 0.0f;
+
+}
+
+void LoadData()
+{
+	ifstream inFile;
+	inFile.open("MapData.bin", ios::in | ios::binary);
+	if (!inFile)
+	{
+		cout << "File didn't load.\n";
+		return;
+	}
+	//cout << "loading entities...\n";
+	inFile.seekg(0, ios::beg);
+	unsigned int numEntities;
+
+	//get the number of entities
+	inFile.read(reinterpret_cast<char*>(&numEntities), sizeof(unsigned int));
+
+	//position now at 4;
+	//cout << "position now at: " << inFile.tellg() << endl;
+
+	//cout << "num: " << numEntities << endl;
+	//loop through and load entity
+	for (unsigned int i = 0; i < numEntities; i++)
+	{
+		//each entity is 20 bytes of 32 byte structure
+		//2 byte
+		Ushort id;
+		inFile.read(reinterpret_cast<char*>(&id), sizeof(Ushort));
+		//2 byte
+		Ushort typeId;
+		inFile.read(reinterpret_cast<char*>(&typeId), sizeof(Ushort));
+		Entity::Type type = static_cast<Entity::Type>(typeId);
+		//4 byte X 2
+		float posX, posY;
+		inFile.read(reinterpret_cast<char*>(&posX), sizeof(float));
+		inFile.read(reinterpret_cast<char*>(&posY), sizeof(float));
+		//4 byte X 2
+		float forwX, forwY;
+		inFile.read(reinterpret_cast<char*>(&forwX), sizeof(float));
+		inFile.read(reinterpret_cast<char*>(&forwY), sizeof(float));
+		entities.push_back(new Entity(id, type, posX, posY, forwX, forwY, GetCollisionRadius(type)));
+		//cout << "loaded entity #" << i << endl;
+		entities.back()->PrintIt();
+
+		//cout << "position now at: " << inFile.tellg() << endl;
+		//advance the 12 bytes (32 byte total - 16 byte for data) to next entity
+		//cout << "advancing 12 byte...\n";
+		inFile.seekg(12, ios::cur);
+	}
+	//cout << "done loading...\n";
+
+	inFile.close();
+}
+
+
+
+void Cleanup()
+{
+	//delete entities created on heap.
+	for (int i = 0; i < entities.size(); i++)
+	{
+		delete entities[i];
+	}
+	entities.clear();
+}
 
 void main()
 {
-	Entity chest = Entity(-1, Entity::Type::CHEST, 0, 0, 1, 0);
-	Entity monster = Entity(1, Entity::Type::OGRE, 10, 10, 0, 1);
-	Entity imp = Entity(2, Entity::Type::IMP, 20, 20, 1, 0);
-	chest.PrintIt();
+	LoadData();
 
+	Cleanup();
 	system("pause");
 
 }
